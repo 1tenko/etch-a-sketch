87ad559e1181c723702e8515d9a30a6692c4ae83
1tenko <tajm.ogunbiyi@gmail.com>
2022-04-14 16:59:20 +0100

    project now works. Bug fixed where changeTileColor() wouldn't work when button was clicked.

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const container = document.querySelector('.container');
-
-let num = 100;
-
-container.style.cssText = `grid-template-columns: repeat(${num},1fr); grid-template-rows: repeat(${num},1fr);`;
+const button = document.querySelector('button');
+const size = 16;
 
 
-for (i=1; i<=num*num; i++) {
+function createGrid(num) {
+    container.innerHTML = '';
+    container.style.cssText = `grid-template-columns: repeat(${num},1fr); grid-template-rows: repeat(${num},1fr);`;
+
+    for (i=1; i<=num*num; i++) {
     const square = document.createElement('div');
     square.setAttribute('class', 'gridItem');
     container.appendChild(square);
+    changeTileColor();
+    }
+}
+createGrid(size);
+
+
+
+
+function changeTileColor() {
+    const gridItems = document.querySelectorAll('.gridItem');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.cssText = 'background-color: black;';
+            console.log('change')
+        })
+    });
 }
 
-const gridItems = document.querySelectorAll('.gridItem');
 
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener('mouseover', event => {
-        gridItem.style.cssText = 'background-color: black;';
-    })
+button.addEventListener('click', () => {
+    let newNum = prompt('Please select new grid size (e.g. entered value of 24 = 24x24 grid: )');
+    console.log(newNum);
+    createGrid(newNum);
 })
